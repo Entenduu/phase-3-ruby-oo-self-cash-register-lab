e32b84ee8252e23fc1a3d77c663cbeb85f4200f2
Cristian <Entenduu@gmail.com>
2022-06-29 20:02:34 -0400

    commit tree e234423fd4fd29e7ceb459bf6fc04fa99ab78274

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,24 @@
+class CashRegister
+    attr_reader :discount
+    attr_accessor :total, :cart
+    
 
+def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+end
+
+
+def add_item(title, price, quantity = nil)
+quantity != nil ? self.total += (price * quantity) : self.total += price
+cart = []
+cart.push(title)
+end
+
+def apply_discount
+    self.discount != nil ? "After the discount, the total comes to $#{self.total -= (self.discount * 10)}." : "There is no discount to apply."
+end
+def items
+    self.cart
+end
+end
